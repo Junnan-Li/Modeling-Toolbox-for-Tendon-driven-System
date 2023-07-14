d9612401a5a880d66b4696fa9a0000123c6207c2
JunnanLi_XMG_win <junnan.li@tum.de>
2023-07-14 17:07:30 +0200

    script move

--- optimization/scripts/ellispe_polytope.m
+++ optimization/scripts/ellispe_polytope.m
@@ -12,14 +12,14 @@ clc
 %%
 
 % J = rand(2,2);
-J = [0.6,0.4;0.2,0.6];
+J = [-0.7071,-0.7071;1.7071,0.7071];
 
 [U,S,V] = svd(J);
 
 %%
 % inequalities for constraints of torque
 H_1 = [eye(2);-eye(2)];
-B_1 = [[1;1]; [1;1]];
+B_1 = [[1;1]; [0;0]];
 
 
 % mpt toolbox define a polytope
@@ -44,7 +44,7 @@ XY = J * [x_input;y_input];
 h = figure(15);
 set(0,'defaultfigurecolor','w')
 set(groot,'defaulttextinterpreter','none');
-% set(h,'units','normalized','outerposition',[0 0 1 1])
+set(h,'units','normalized','outerposition',[0 0 1 1])
 set(h, 'Units', 'centimeters')
 pos = get(h,'Position');
 set(h,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)]);%
@@ -61,13 +61,18 @@ hold on
 P_input_poly.plot('alpha',0,'LineWidth',4,'edgecolor','b')
 axis equal
 grid off
-set(gca,'xticklabel',[],'xtick',[])
-set(gca,'yticklabel',[],'ytick',[])
-set(gca,'Visible','off')
+xticks([-1 0 1])
+yticks([-1 0 1])
+% set(gca,'xticklabel',[],'xtick',[])
+% set(gca,'yticklabel',[],'ytick',[])
+% set(gca,'Visible','off')
 
 subplot(1,2,2)
-plot(XY(1,:)',XY(2,:)' ,'r-','LineWidth',4)
+plot([0;1;1+sqrt(2)/2],[0;0;sqrt(2)/2],'o-', 'LineWidth',4,'Color','k','MarkerSize',18)
 hold on
+plot(XY(1,:)'+1+sqrt(2)/2,XY(2,:)' + sqrt(2)/2,'r-','LineWidth',4)
+hold on
+P_res = P_res+[1+sqrt(2)/2,sqrt(2)/2]';
 P_res.plot('alpha',0,'LineWidth',4,'edgecolor','b')
 axis equal
 grid off
