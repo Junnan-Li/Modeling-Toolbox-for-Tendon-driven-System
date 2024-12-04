108e266bcc056db1de4eebf1367d5139d391f198
Junnan-Li <junnan.li@tum.de>
2024-12-04 17:09:12 +0100

    update sim functions

--- test/test_simulink_functions.m
+++ test/test_simulink_functions.m
@@ -146,6 +146,77 @@ end
 fprintf('Hand ID Time cost: \n')
 fprintf('invdyn_ne_T:            %f \n',t1/iter)
 fprintf('------------------------ \n')
+%%
+
+hand.create_sim_functions_hand
+test_failed = 0;
+% t1 = 0;
+for iter = 1:100
+    q = rand(hand.nj,1);
+    [w_T_all_frames,w_T_all_links] = sim_w_T_all_frames_from_q(hand.sim.mdh,hand.sim.mdh_index,...
+        hand.sim.q_index,hand.sim.w_T_b,hand.sim.n_links,q);
+    [w_T_all_frames_t,w_T_all_links_t] = hand_function_test_sim_w_T_all_frames_from_q(q);
+    
+    T_f_error = w_T_all_frames-w_T_all_frames_t;
+    T_l_error = w_T_all_links - w_T_all_links_t;
+    if max(abs([T_f_error(:);T_l_error(:)])) > 1e-8
+        test_failed = 1;
+    end
+end
+if test_failed
+    fprintf('Test (sim transformation function): failed! \n')
+else
+    fprintf('Test (sim transformation function): pass! \n')
+end
+fprintf('------------------------ \n')
+
+%% sim func inverse dynamic 
+
+test_failed = 0;
+t1 = 0;
+t2 = 0;
+for iter = 1:100
+    q = rand(hand.nj,1);
+    qd = rand(hand.nj,1);
+    qdd = rand(hand.nj,1);
+    hand.update_hand(q);
+    hand.update_sim_par;
+    hand.update_hand_par_dyn;
+    hand_rst = hand.update_rst_model;
+    w_T_all_frames = sim_w_T_all_frames_from_q(hand.sim.mdh,hand.sim.mdh_index,...
+        hand.sim.q_index,hand.sim.w_T_b,hand.sim.n_links,q);
+
+    X_base = zeros(6,1);
+    X_base(1:3) = hand.w_p_base;
+    X_base(4:6) = R2euler_XYZ(hand.w_R_base);
+    XD_base = zeros(6,1);
+    XDD_base = zeros(6,1);
+    F_ext = zeros(6,hand.nf);
+    tic
+    tau_obj = invdyn_ne_T(w_T_all_frames,qd,qdd, hand.sim.n_links, hand.sim.q_index, ...
+        hand.sim.Mass, X_base, XD_base,XDD_base, F_ext, hand.sim.CoM, hand.sim.I, hand.sim.g);
+    t1 = t1 + toc;
+    tic
+    tau_sim = hand_function_test_invdyn_ne_T(w_T_all_frames,qd,qdd, X_base, XD_base,XDD_base, F_ext);
+    t2 = t2 + toc;
+    tau_rst = hand_rst.inverseDynamics(q,qd,qdd);
+    error_tau = max(abs(tau_obj - tau_sim));
+    error_tau2 = max(abs(tau_obj - tau_rst));
+    if max(abs(error_tau)) > 1e-8
+        test_failed = 1;
+    end
+end
+if test_failed
+    fprintf('Test (Inverse dynamic): failed! \n')
+else
+    fprintf('Test (Inverse dynamic): pass! \n')
+end
+fprintf('Hand ID Time cost: \n')
+fprintf('invdyn_ne_T    :            %f \n',t1/iter)
+fprintf('invdyn_ne_T sim:            %f \n',t2/iter)
+fprintf('------------------------ \n')
+
+return
 %% Test forward dynamic and individual terms
 % 
 
@@ -161,10 +232,10 @@ hand_rst = hand.update_rst_model;
 
 
 [qDD,M,C,G] = hand.fordyn_ne_hand_w_end(q);
-M_sym = invdyn_lag_T_M(w_T_all_links, hand.sim.Mass, hand.sim.CoM, hand.sim.I)
-error_M = double(M_sym) - M
-max(error_M(:))
+M_sym = invdyn_lag_T_M(w_T_all_links, hand.sim.Mass, hand.sim.CoM, hand.sim.I);
+error_M = double(M_sym) - M;
+max(error_M(:));
 
-
+M_rst = hand_rst.massMatrix
 
 
